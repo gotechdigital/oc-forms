716269c4e74ca173e59925bc789896af46328e12
Lucas Zamora <lucaszdv@gmail.com>
2022-11-20 11:07:08 -0300

    Create tables

--- updates/create_records_table.php
+++ updates/create_records_table.php
@@ -9,14 +9,21 @@ use October\Rain\Database\Updates\Migration;
 class CreateRecordsTable extends Migration {
 
     public function up() {
-        Schema::create('gotech_forms_records', function (Blueprint $table) {
-            $table->engine = 'InnoDB';
-            $table->increments('id');
-            $table->text('form_data')->nullable();
-            $table->string('ip')->nullable();
-            $table->softDeletes();
-            $table->timestamps();
-        });
+        
+        if (Schema::hasTable('martin_forms_records')) {
+            Schema::rename('martin_forms_records', 'gotech_forms_records');
+        } else {
+            Schema::create('gotech_forms_records', function (Blueprint $table) {
+                $table->engine = 'InnoDB';
+                $table->increments('id');
+                $table->string('group')->nullable();
+                $table->text('form_data')->nullable();
+                $table->string('ip')->nullable();
+                $table->boolean('unread')->default(1);
+                $table->softDeletes();
+                $table->timestamps();
+            });
+        }
     }
 
     public function down() {
